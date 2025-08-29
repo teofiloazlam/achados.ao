3fc17c15d4dfae1b26c3f29f0e797b855e70c787
Repolho_Preto <teofiloseco43@gmail.com>
2025-08-29 12:25:50 -0300

    adicionando swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe())
-  await app.listen(process.env.PORT ?? 6000);
+
+  // 🔹 Pipes globais (validação)
+  app.useGlobalPipes(new ValidationPipe());
+
+  // 🔹 Configuração Swagger
+  const config = new DocumentBuilder()
+    .setTitle('API Achados e Perdidos') // título da documentação
+    .setDescription('Documentação da API para gerenciamento de documentos achados/perdidos') // descrição
+    .setVersion('1.0') // versão
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document); // rota será /api
+
+  // 🔹 Porta dinâmica (Render usa process.env.PORT)
+  await app.listen(process.env.PORT ?? 8000);
 }
 bootstrap();
